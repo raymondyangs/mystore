35467390487921f2e6e8cfd0739bccd7e5e8d668
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2017-06-19 02:13:50 +0800

    Implement product image upload

--- estore/urls.py
+++ estore/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,3 +9,6 @@ urlpatterns = [
     url(r'^create$', views.ProductCreate.as_view(), name='product_create'),
     url(r'^(?P<pk>\d+)/update$', views.ProductUpdate.as_view(), name='product_update'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
