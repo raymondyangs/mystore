0559fd94b5a3541daf20955f9097d70101d681fb
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2017-06-19 01:45:38 +0800

    Use generic editing views

--- estore/urls.py
+++ estore/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
-    url(r'^create$', views.product_create, name='product_create'),
-    url(r'^(?P<pk>\d+)/update$', views.product_update, name='product_update'),
+    url(r'^$', views.ProductList.as_view(), name='product_list'),
+    url(r'^create$', views.ProductCreate.as_view(), name='product_create'),
+    url(r'^(?P<pk>\d+)/update$', views.ProductUpdate.as_view(), name='product_update'),
 ]
--- estore/views.py
+++ estore/views.py
@@ -1,38 +1,33 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
 
-from .forms import ProductForm
 from .models import Product
 
 
 # Create your views here.
 
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'estore/product_list.html', {'products': products})
+class ProductList(generic.ListView):
+    model = Product
 
 
-@permission_required('estore.add_product')
-def product_create(request):
-    if request.method == "POST":
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            product = form.save()
-            return redirect('product_list')
-    else:
-        form = ProductForm()
-    return render(request, 'estore/product_form.html', {'form': form})
+class ProductCreate(PermissionRequiredMixin, generic.CreateView):
+    permission_required = 'estore.add_product'
+    model = Product
+    fields = ('title', 'description', 'quantity', 'price')
+
+    def get_success_url(self):
+        messages.success(self.request, '產品已新增')
+        return reverse('product_list')
 
 
-@permission_required('estore.change_product')
-def product_update(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    form = ProductForm(request.POST or None, instance=product)
+class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
+    permission_required = 'estore.change_product'
+    model = Product
+    fields = ('title', 'description', 'quantity', 'price')
 
-    if request.POST and form.is_valid():
-        form.save()
-        messages.success(request, '產品已變更')
-
-    return render(request, 'estore/product_form.html', {'form': form})
+    def get_success_url(self):
+        messages.success(self.request, '產品已變更')
+        return reverse('product_update', kwargs=self.kwargs)
