b7d31f15da9b9456a8b5d96122572f0c875ceb07
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2017-08-23 04:25:13 +0800

    Add django-naomi

--- mystore/settings.py
+++ mystore/settings.py
@@ -131,3 +131,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+INSTALLED_APPS += ('naomi',)
+EMAIL_BACKEND = 'naomi.mail.backends.naomi.NaomiBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'mail')
