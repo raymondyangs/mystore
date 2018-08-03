cd302f7cff937d86a2f7c97d10ffd19d77c9fa03
cjltsod <github.tsod@tsod.idv.tw>
2018-08-03 18:13:43 +0800

    Use generic views

--- estore/urls.py
+++ estore/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('create', views.product_create, name='product_create'),
-    path('<int:pk>/update', views.product_update, name='product_update'),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('create', views.ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update', views.ProductUpdate.as_view(), name='product_update'),
 ]
