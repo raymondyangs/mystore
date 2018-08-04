3aab267858110e8559b1e7d21285176c198674a5
cjltsod <github.tsod@tsod.idv.tw>
2018-08-04 09:18:12 +0800

    Add user management

--- estore/urls.py
+++ estore/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('dashboard/products/', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'), name='dashboard_product_list'),
     path('dashboard/products/create', views.ProductCreate.as_view(), name='dashboard_product_create'),
     path('dashboard/products/<int:pk>/update', views.ProductUpdate.as_view(), name='dashboard_product_update'),
+
+    path('dashboard/users/', views.UserList.as_view(), name='dashboard_user_list'),
+    path('dashboard/users/<int:pk>/addtostaff', views.UserAddToStaff.as_view(), name='dashboard_user_addtostaff'),
+    path('dashboard/users/<int:pk>/removefromstaff', views.UserRemoveFromStaff.as_view(), name='dashboard_user_removefromstaff'),
 ]
 
 if settings.DEBUG:
