16dd99674d1008060865f7885f6d965599dc8ad0
cjltsod <github.tsod@tsod.idv.tw>
2018-08-06 09:44:50 +0800

    Add shopping cart button and set price css

--- mystore/settings.py
+++ mystore/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'estore',
     'bootstrap4',
+    'fontawesome',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
