1b23464adfd4c73e5d83fe8736311bf45316998a
cjltsod <github.tsod@tsod.idv.tw>
2018-11-24 04:55:05 +0800

    Add backstage

--- estore/urls.py
+++ estore/urls.py
@@ -6,9 +6,11 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
-    path('create', views.ProductCreate.as_view(), name='product_create'),
     path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('<int:pk>/update', views.ProductUpdate.as_view(), name='product_update'),
+
+    path('dashboard/products/', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'), name='dashboard_product_list'),
+    path('dashboard/products/create', views.ProductCreate.as_view(), name='dashboard_product_create'),
+    path('dashboard/products/<int:pk>/update', views.ProductUpdate.as_view(), name='dashboard_product_update'),
 ]
 
 if settings.DEBUG:
