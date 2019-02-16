1bd3294e24ce55bb9394055d629e9c3c4773c2d3
Raymond Yang <thelife1988@gmail.com>
2019-02-16 11:57:38 +0800

    Add order creating

--- estore/urls.py
+++ estore/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
     path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
     path('cart/', views.CartDetailFromRequest.as_view(), name='cart_detail'),
+    path('cart/checkout', views.OrderCreateCartCheckout.as_view(), name='cart_checkout'),
     path('dashboard/products/', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'), name='dashboard_product_list'),
     path('dashboard/products/create', views.ProductCreate.as_view(), name='dashboard_product_create'),
     path('dashboard/products/<int:pk>/update', views.ProductUpdate.as_view(), name='dashboard_product_update'),
